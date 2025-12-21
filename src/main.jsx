aba8f726a5e650017d9b71591913787277d53107
aravindjammula <jammulaaravind027@gmail.com>
2025-12-21 11:46:39 +0530

    Switch to HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom' // Changed from BrowserRouter
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* ADD THIS basename="/react" */}
-    <BrowserRouter basename="/react">
+    {/* HashRouter handles GitHub Pages paths perfectly */}
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
